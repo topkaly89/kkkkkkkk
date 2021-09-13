7c2a05f3fd75c6fbeabfd5508c73bb6931ed02a2
topkaly89 <allmylove00@gmail.com>
2021-09-13 17:55:57 +0700

    Add files via upload

---
+++ components/Section2.js
@@ -0,0 +1,105 @@
+import { useEffect, useState } from 'react'
+import Parallax from "parallax-js"
+import { motion } from 'framer-motion';
+
+const layers = [
+    {
+        name: "planet_a",
+        image: '/planet_a.svg',
+        dataDepth: "1",
+        position: "right",
+        y: "1/2",
+        x: 10,
+        size: 300,
+        rotate: 45
+    },
+    {
+        name: "planet_a2",
+        image: '/planet_b.svg',
+        dataDepth: "2",
+        position: "left",
+        y: "1/3",
+        x: 10,
+        size: 150,
+        rotate: 0
+    },
+    {
+        name: "planet_a3",
+        image: '/planet_a.svg',
+        dataDepth: "1",
+        position: "left",
+        y: "3/4",
+        x: 20,
+        size: 80,
+        rotate: 12
+    }
+];
+
+export default function Section2(props) {
+    useEffect(() => {
+        const scene1 = document.getElementById("scene-1")
+        new Parallax(scene1)
+        const scene2 = document.getElementById("scene-2")
+        new Parallax(scene2)
+        const scene3 = document.getElementById("scene-3")
+        new Parallax(scene3)
+    })
+
+    const [isHovered, setHovered] = useState(false)
+
+    return (
+        <div className="section w-full">
+            <div className="section-header px-30 flex px-44 absolute top-0 py-10 lg:py-20 justify-between w-full z-50 flex-col md:flex-row items-center">
+                <div className="section-title text-3xl lg:text-6xl flex items-center">
+                    CONTACT
+                </div>
+                <div className="section-title normal-text flex items-center text-gray-200 hover:text-white transition-all text-lg lg:text-2xl">
+                    <a href="mailto:contact@astrov.ventures">contact@astrov.ventures</a>
+                </div>
+            </div>
+            <div className="w-full h-screen absolute top-0 left-0 z-10">
+                {
+                    layers.map((l, index) => (
+                        <div key={index} className={`absolute md:block hidden`} id={`scene-${index+1}`}>
+                            <img
+                                data-depth={l.dataDepth}
+                                src={l.image}
+                                alt={l.name}
+                                width={l.size+"px"}
+                            />
+                        </div>
+                    ))
+                }
+            </div>
+            <div className="section-body w-full h-screen flex items-center justify-center">
+                <a className="absolute z-50 cursor-pointer w-5/12 md:w-auto flex items-center justify-center" href="mailto:contact@astrov.ventures">
+                    <motion.img
+                        initial={false}
+                        animate={{ y: isHovered ? -30 : 0 }}
+                        transition={{ ease: "easeOut", duration: 0.5 }}
+                        src="/space_ship.svg"
+                        onMouseEnter={() => setHovered(true)}
+                        onMouseLeave={() => setHovered(false)}
+                    />
+                </a>
+                <img src="/big_planet.svg" className="absolute z-30" />
+                <a
+                    className="absolute z-40 flex items-center justify-center" href="mailto:contact@astrov.ventures"
+                    onMouseEnter={() => setHovered(true)}
+                    onMouseLeave={() => setHovered(false)}
+                >
+                    <motion.img
+                        src="/normal_planet.svg" className="w-8/12 md:w-auto cursor-pointer"
+                    />
+                    <motion.div
+                        initial={{ opacity: 0.6 }}
+                        animate={{ opacity: isHovered ? 1 : 0.6 }}
+                        className="contact-now normal-text z-50 text-2xl lg:text-4xl font-extrabold absolute bottom-5 lg:bottom-10 text-center w-full"
+                    >
+                        NOW
+                    </motion.div>
+                </a>
+            </div>
+        </div>
+    )
+}
